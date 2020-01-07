4065092e7cc067594a2f47a85e055ba5efd18437
kssi0040 <admin@kssi.net>
2020-01-07 10:07:53 +0900

    Project Upload
    Upload Project

---
+++ Assets/Script/RotateGame.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RotateGame : MonoBehaviour
+{
+    public StagePlay m_StagePlay;
+    public List<GameObject> Statue=new List<GameObject>();
+    public List<bool> Clear = new List<bool>();
+    public int Count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Statue.Count!=0)
+        {
+            for(int k=0;k<Statue.Count;k++)
+            {
+                Statue[k].transform.GetChild(3).gameObject.SetActive(false);
+                Clear.Add(false);
+            }
+        }
+        m_StagePlay = GameObject.Find("StagePlay").GetComponent<StagePlay>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.touchCount>0)
+        //{
+        //    Touch touch = Input.GetTouch(0);
+
+        //    if(EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+        //    {
+        //        return;
+        //    }
+        //    ObjectSelect();
+        //}        
+        if (Input.GetMouseButtonDown(0))
+        {
+            ObjectSelect();
+        }
+
+        for (int k = 0; k < Statue.Count; k++)
+        {
+            if (Statue[k].transform.GetChild(0).GetComponent<StatueMove>().Clear == true)
+            {
+                Clear[k] = true;               
+            }
+            else
+            {
+                Clear[k] = false;                
+            }
+        }
+        Count = 0;
+        for(int k=0;k<Clear.Count;k++)
+        {
+            if(Clear[k]==true)
+            {
+                Count++;
+            }
+        }
+        if (Count == Clear.Count)
+        {
+            PlayerInfo.Instance.isComplite = true;            
+        }
+        else
+        {
+            PlayerInfo.Instance.isComplite = false;
+        }
+
+        for(int k=0;k<Statue.Count;k++)
+        {
+            if (Statue[k].transform.GetChild(0).GetComponent<StatueMove>().Left==true|| Statue[k].transform.GetChild(0).GetComponent<StatueMove>().Right == true)
+            {
+                Statue[k].transform.GetChild(2).gameObject.SetActive(true);                
+            }
+            else
+            {
+                Statue[k].transform.GetChild(2).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void ObjectSelect()
+    {        
+        //if(Input.touchCount==1)
+        //{
+            Debug.Log("Start");
+            RaycastHit hit;
+        //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        bool bCheck = Physics.Raycast(ray, out hit, 30.0f);
+            if(bCheck==true)
+            {
+                string name = hit.collider.gameObject.name;
+
+                foreach(GameObject obj in Statue)
+                {
+                    if(obj.transform.gameObject.name==name)
+                    {                    
+                        obj.transform.GetChild(3).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        obj.transform.GetChild(3).gameObject.SetActive(false);
+                    }
+                }
+            }
+        //}
+    }
+}
---
+++ Assets/Script/StatueMove.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatueMove : MonoBehaviour
+{
+    public bool Left = false;
+    public bool Right = false;   
+    public float speed;
+    public float Answer;
+    public GameObject Particle;
+    public bool Clear=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Particle!=null)
+        {
+            Particle.SetActive(false);
+        }
+        this.transform.rotation = Quaternion.Euler(this.transform.rotation.x, Random.Range(0.0f, 180.0f), 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Left==true)
+        {
+            this.transform.Rotate(0.0f, (-90.0f * Time.deltaTime) / speed, 0.0f);
+        }
+
+        if(Right==true)
+        {
+            this.transform.Rotate(0.0f, (90.0f * Time.deltaTime) / speed, 0.0f);
+        }
+        
+        if(Particle!=null)
+        {
+            if(this.transform.eulerAngles.y<=Answer+3.0f&&this.transform.eulerAngles.y >=Answer-3.0f)
+            {
+                if(Particle.activeSelf==false)
+                {
+                    Particle.SetActive(true);
+                    Clear = true;
+                }
+            }           
+
+            else if(this.transform.eulerAngles.y<=360.0-Answer+3.0f&&this.transform.eulerAngles.y>=360-Answer-3.0f)
+            {
+                if(Particle.activeSelf==false)
+                {
+                    Particle.SetActive(true);
+                    Clear = true;
+                }
+            }
+            else
+            {
+                Particle.SetActive(false);
+                Clear = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+    }
+
+    public void LeftButtonDown()
+    {
+        Left = true;
+    }
+
+    public void LeftButtonUp()
+    {
+        Left = false;
+    }
+
+    public void RightButtonDown()
+    {
+        Right = true;
+    }
+
+    public void RightButtonUp()
+    {
+        Right = false;
+    }
+}
